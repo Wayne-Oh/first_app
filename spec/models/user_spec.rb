e58c88a6afc7d61cda9fa72a3ce168e1276e2a4f
Wayne Johnson <wayneo.johnson854@gmail.com>
2012-06-18 12:06:20 -0700

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid }
@@ -89,7 +90,7 @@ describe User do
 
     describe "with valid password" do
       it { should == found_user.authenticate(@user.password) }
-   end
+    end
 
     describe "with invalid password" do
       let(:user_for_invalid_password) { found_user.authenticate("invalid") }
@@ -98,4 +99,10 @@ describe User do
       specify { user_for_invalid_password.should be_false }
     end
   end
+  
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 end
