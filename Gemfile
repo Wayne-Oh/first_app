76374fbbbb43a81ce04d11777a9dc3e83f40d890
Wayne Johnson <wayneo.johnson854@gmail.com>
2012-06-21 13:43:50 -0700

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -4,9 +4,12 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.3'
 gem 'bootstrap-sass', '2.0.0'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'faker', '1.0.1'
+gem 'will_paginate', '3.0.3'
+gem 'bootstrap-will_paginate', '0.0.6'
 
 group :develoment, :test do
-#  gem 'sqlite3', '1.3.6'
+  gem 'sqlite3', '1.3.6'
   gem 'pg', '0.12.2'
   gem 'rspec-rails', '2.9.0'
   gem 'annotate', '~> 2.4.1.beta'
--- spec/factories.rb
+++ spec/factories.rb
@@ -5,10 +5,13 @@ FactoryGirl.define do
   # we tell Factory Girl that the subsequent definition 
   # is for a User model object.
   factory :user do
-    name     "Michael Hartl"
-    email    "michael@example.com"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
     password "foobar"
     password_confirmation "foobar"
-  end
 
+    factory :admin do
+      admin true
+    end
+  end
 end
